2bdbe57789a85deca7c77339f4d3b4b5ae2e1ca9
PozdeevaElizaveta <Tyuiu.PozdeevaEA@outlook.com>
2025-11-23 20:05:29 +0500

    Task6

---
+++ Tyuiu.PozdeevaEA.Sprint5.Task6.V22.Lib/DataService.cs
@@ -0,0 +1,28 @@
+﻿using System.Text;
+using tyuiu.cources.programming.interfaces.Sprint5;
+
+namespace Tyuiu.PozdeevaEA.Sprint5.Task6.V22.Lib
+{
+    public class DataService : ISprint5Task6V22
+    {
+        public int LoadFromDataFile(string path)
+        {
+            int count = 0;
+            using (StreamReader reader = new StreamReader(path))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    for (int i = 0; i < line.Length - 1; i++)
+                    {
+                        if (line[i] == 'м' && line[i + 1] == 'м')
+                        {
+                            count++;
+                        }
+                    }
+                }
+            }
+            return count;
+        }
+    }
+}
---
+++ Tyuiu.PozdeevaEA.Sprint5.Task6.V22.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.PozdeevaEA.Sprint5.Task6.V22.Lib;
+namespace Tyuiu.PozdeevaEA.Sprint5.Task6.V22.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            string path = @"C:\Users\lpozd\source\repos\Tyuiu.PozdeevaEA.Sprint5\Tyuiu.PozdeevaEA.Sprint5.Task6.V22\bin\Debug\net8.0\InPutDataFileTask6V22.txt";
+
+            var res = ds.LoadFromDataFile(path);
+            int wait = 2;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
