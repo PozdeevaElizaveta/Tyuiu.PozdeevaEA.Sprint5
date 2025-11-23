317e44b80c0b853867d218dedacc3e7b0394f71f
PozdeevaElizaveta <Tyuiu.PozdeevaEA@outlook.com>
2025-11-23 19:28:26 +0500

    Task5

---
+++ Tyuiu.PozdeevaEA.Sprint5.Task5.V25.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.PozdeevaEA.Sprint5.Task5.V25.Lib;
+namespace Tyuiu.PozdeevaEA.Sprint5.Task5.V25.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string path = @"C:\Users\lpozd\AppData\Local\Temp\tmpxhjsb5.tmp";
+            FileInfo fileInfo = new FileInfo(path);
+            bool file = fileInfo.Exists;
+            bool wait = true;
+            Assert.AreEqual(wait, file);
+        }
+    }
+}
---
+++ Tyuiu.PozdeevaEA.Sprint5.Task5.V25/Program.cs
@@ -0,0 +1,38 @@
+﻿using Tyuiu.PozdeevaEA.Sprint5.Task5.V25.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+
+        Console.Title = "Спринт #5 | Выполнил: Поздеева Е. А. | ПКТб-25-1";
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #5                                                               *");
+        Console.WriteLine("* Тема: Чтение данных из текстового файла                                 *");
+        Console.WriteLine("* Задание #5                                                              *");
+        Console.WriteLine("* Вариант #25                                                             *");
+        Console.WriteLine("* Выполнил: Поздеева Елизавета Александровна | ПКТб-25-1                  *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Дан файл, в котором есть набор значений. Найти произведение всех        *");
+        Console.WriteLine("* вещественных чисел в файле. Результат округлить до 3 знаков после       *");
+        Console.WriteLine("* запятой и вернуть полученный результат на консоль.                      *");
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        string path = @"C:\Users\lpozd\source\repos\Tyuiu.PozdeevaEA.Sprint5\Tyuiu.PozdeevaEA.Sprint5.Task5.V25\bin\Debug\net8.0\InPutDataFileTask5V25.txt";
+        Console.WriteLine("Исходный файл с данными: " + path);
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        double res = ds.LoadFromDataFile(path);
+        Console.WriteLine("Произведение элементов файла" + res);
+
+        Console.ReadKey();
+    }
+}
