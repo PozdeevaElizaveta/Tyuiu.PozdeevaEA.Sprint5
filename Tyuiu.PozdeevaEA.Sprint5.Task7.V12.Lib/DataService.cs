6ce5cf2c111240293c34bf8d1ceb49a72f0f757d
PozdeevaElizaveta <Tyuiu.PozdeevaEA@outlook.com>
2025-11-23 20:34:32 +0500

    Task7

---
+++ Tyuiu.PozdeevaEA.Sprint5.Task7.V12.Lib/DataService.cs
@@ -0,0 +1,32 @@
+﻿using System.Text;
+using tyuiu.cources.programming.interfaces.Sprint5;
+using tyuiu.cources.programming.interfaces.Sprint6;
+namespace Tyuiu.PozdeevaEA.Sprint5.Task7.V12.Lib
+{
+    public class DataService : ISprint5Task7V12
+    {
+        public string LoadDataAndSave(string path)
+        {
+            string content = File.ReadAllText(path);
+            StringBuilder result = new StringBuilder();
+            foreach (char c in content)
+            {
+                if (c >= 'а' && c <= 'я')
+                {
+                    result.Append(char.ToUpper(c));
+                }
+                else
+                {
+                    result.Append(c);
+                }
+            }
+
+            string transformedText = result.ToString();
+
+            string outputPath = @"C:\Users\lpozd\source\repos\Tyuiu.PozdeevaEA.Sprint5\Tyuiu.PozdeevaEA.Sprint5.Task7.V12\bin\Debug\net8.0\OutPutDataFileTask7V12.txt";
+            File.WriteAllText(outputPath, transformedText);
+
+            return transformedText;
+        }
+    }
+}
---
+++ Tyuiu.PozdeevaEA.Sprint5.Task7.V12.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.PozdeevaEA.Sprint5.Task7.V12.Lib;
+namespace Tyuiu.PozdeevaEA.Sprint5.Task7.V12.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string path = @"C:\Users\lpozd\source\repos\Tyuiu.PozdeevaEA.Sprint5\Tyuiu.PozdeevaEA.Sprint5.Task7.V12\bin\Debug\net8.0\InPutDataFileTask7V12.txt";
+            FileInfo fileInfo = new FileInfo(path);
+            bool file = fileInfo.Exists;
+            bool wait = true;
+            Assert.AreEqual(wait, file);
+        }
+    }
+}
